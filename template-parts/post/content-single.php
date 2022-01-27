1c032c3f4700fc1bf6366fad40d11e8810c369f9
xavierjeanne <xavier.jeanne@gmail.com>
2022-01-27 21:22:55 +0100

    add type

--- template-parts/post/content-single.php
+++ template-parts/post/content-single.php
@@ -1,19 +1,24 @@
 <a href="<?php echo site_url();?>">Retour à l'accueil</a>
 <h2><?php the_title();?></h2>
 <p><?php the_field('parole');?></p>
+<hr>
 <?php if( have_rows('extrait') ): ?>
-    <ul class="slides">
+    <div class="slides row">
     <?php while( have_rows('extrait') ): the_row(); 
         $fichier = get_sub_field('fichier');
+        $type = get_sub_field('type_dextrait');
         ?>
-       <figure>
-            <audio
-                controls
-                src="<?php echo $fichier;?>">
-                    Your browser does not support the
-                    <code>audio</code> element.
-            </audio>
-        </figure>
+        <div class="col-12 col-md-6">type d'extrait : <?php echo $type;?></div>
+        <div class="col-12 col-md-6">
+            <figure>
+                <audio
+                    controls
+                    src="<?php echo $fichier;?>">
+                        Your browser does not support the
+                        <code>audio</code> element.
+                </audio>
+            </figure>
+        </div>
     <?php endwhile; ?>
-    </ul>
+    </div>
 <?php endif; ?>
